6e81dafea8f54f79bb7ba348e455e765197197ab
= <=>
2022-06-06 19:17:51 +0530

    created category route and initialised the database

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,1 +1,23 @@
-app.post("/ecomm/api/v1/categories")
+/**
+ * This file will contain the routing logic for the Category Controller
+ *
+ */
+
+const categoryController = require("../controllers/category.controller")
+
+module.exports = (app) => {
+    //Route for the POST request to create a category
+    app.post("/ecom/api/v1/categories", categoryController.create)
+
+    //Route for the GET request to fetch all the categories
+    app.get("/ecom/api/v1/categories", categoryController.findAll)
+
+    //Route for the GET request to fetch a category based on category id
+    app.get("/ecom/api/v1/categories/:id", categoryController.findOne)
+
+    //Route for the PUT request to update a category based on id
+    app.put("/ecom/api/v1/categories/:id", categoryController.update)
+
+    //Route for the DELETE request to delete a category based on id
+    app.delete("/ecom/api/v1/categoties/:id", categoryController.delete)
+}
--- server.js
+++ server.js
@@ -1,6 +1,51 @@
 const express = require("express")
 const serverConfig = require("./configs/server.config")
+const bodyParser = require("body-parser")
+
 const app = express()
+
+/**
+ * Using the body parse middleware
+ *
+ * Used for parsing the request
+ * Parsing the request of the type json and convert that to object
+ */
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+/***
+ * Initialize the database
+ */
+
+const db = require("./models")
+const { init } = require("express/lib/application")
+const category = db.category
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("table was dropped and created")
+    init()
+})
+function inti() {
+    var categoties = [
+        {
+            name: "Electronics",
+            description:
+                "This category will contain all the electronic products",
+        },
+        {
+            name: "KitchenItems",
+            description: "This category will contain all the kitchen products",
+        },
+    ]
+    category
+        .bulkCreate(categoties)
+        .then(() => console.log("Category table initialised"))
+        .catch((err) => {
+            console.log("Error while initialising the category table")
+        })
+}
+
+require("./routes/category.routes")(app)
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Listning on port ${serverConfig.PORT}`)
